30f2215dd622d5900abb919f88ab88db345dfc68
Kotlin AB <kotlin@gmx.es>
2022-07-01 15:17:16 +0200

    Commiti Inicial

---
+++ app/src/main/java/es/ipow/shapes/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.ipow.shapes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// /res/values/themes/themes.xml
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
